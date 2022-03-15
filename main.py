6196ac3c6211860ca1722317547a0a3cdcf9934a
piasekUWM <166306@student.uwm.edu.pl>
2022-03-15 21:45:23 +0100

    ZadaniaPython

---
+++ main.py
@@ -0,0 +1,85 @@
+# Zad 1.
+
+# lista = ['żużel', 'skoki narciarskie', 'koszykówka']
+# lista.reverse()
+# print(lista)
+# lista.append('siatkowka')
+# lista.append('unihokej')
+# print(lista)
+
+
+# Zad 2.
+
+# slownik = {'zw':'zaraz wracam',"imo":'in my opinion', 'tbh':'to be honest' }
+# print(slownik['zw'])
+# print(slownik['imo'])
+# print(slownik['tbh'])
+
+
+# Zad 3.
+
+# slownik = {'LOL':'league of legends',"GTA":'grand theft auto', 'CS-GO':'counter strike global offensive' }
+# print(len(dict.keys(slownik)))
+
+
+# Zad 4.
+
+# napis = input("Podaj napis : ")
+# print(napis.count('a'))
+
+
+# Zad 5.
+
+# import sys as system
+
+# system.stdout.write("Podaj trzy liczby całkowite : ")
+# a = system.stdin.readline()
+# a = int(a)
+# b = system.stdin.readline()
+# b = int(b)
+# c = system.stdin.readline()
+# c = int(c)
+# dzialanie = a*b + c
+# dzialanie = str(dzialanie)
+# system.stdout.write(dzialanie)
+
+
+# Zad 6.
+
+# print('Podaj 3 liczby całkowite : ')
+# a = input()
+# b = input()
+# c = input()
+# a = int(a)
+# b = int(b)
+# c = int(c)
+# print(max(a, b, c))
+
+
+# Zad 7.
+
+# lista = [ 6, 8, 3, 12, 89.1, 5.17, -6.5]
+# for x in lista:
+#     print(x*x)
+
+
+# Zad 8.
+
+# a = 0
+# lista = []
+# while(a < 10):
+#     b = int(input("Podaj liczbe całkowitą : "))
+#     if(b%2==0):
+#         lista.append(b)
+#     a+=1
+# print(lista)
+
+
+# Zad. 9.
+
+# import math
+# a = float(input('Podaj liczbe : '))
+# if(a<0):
+#     print('Podano nieprawidłową wartość')
+# else:
+#     print(math.sqrt(a))
